8475f57976b3c66d60419a07f47c426d58548ff8
EdersonDav <silva.edersonqueiroz@gmail.com>
2021-05-06 00:50:53 -0300

    Create services user and product

---
+++ src/services/ProductService.ts
@@ -0,0 +1,59 @@
+import { getCustomRepository, Repository, Like } from "typeorm";
+import { Product } from "../entities/Product";
+import { ProductsRepository } from "../repositories/ProductRepository";
+
+class ProductsService {
+  private ProductRepository: Repository<Product>
+
+  constructor(){
+    this.ProductRepository = getCustomRepository(ProductsRepository);
+  }
+
+  async create(name:string , description:string, expiredTime:Date, price: number){
+
+    const product = this.ProductRepository.create({ name, description, expiredTime, price });
+    await this.ProductRepository.save(product);
+
+    return product;
+  }
+
+  async getAll(page: number){
+    const take = 10;
+    const skip = (page - 1) * 10;
+    const products = await this.ProductRepository.find({take, skip });
+    if(products){
+      return products;
+    }
+
+    throw new Error ('Products not found')
+  }
+
+  async search(term: string, page: number){
+    const take = 10;
+    const skip = (page - 1) * 10;
+
+    let products = await this.ProductRepository.find({where: {name : term}, take, skip});
+
+    if(products.length){
+      return products
+    }else{
+      products = await this.ProductRepository.find({where: {description : Like(`%${term}%`)}, take, skip});
+      if(products.length){
+        return products
+      }
+    }
+
+    throw new Error ('Products not found')
+  }
+
+  async getById(id: string){
+    const product = await this.ProductRepository.find({where:{id}});
+    if(product){
+      return product;
+    }
+
+    throw new Error ('Product not found')
+  }
+}
+
+export {ProductsService}
---
+++ src/services/UserService.ts
@@ -0,0 +1,31 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UserRepository";
+
+class UsersService {
+  private userRepository: Repository<User>
+
+  constructor(){
+    this.userRepository = getCustomRepository(UsersRepository);
+  }
+
+  async create(name:string , password:string){
+
+    const user = this.userRepository.create({ name, password });
+    await this.userRepository.save(user);
+    return user;
+  }
+
+  async getUser(name: string, password: string){
+    const user = await this.userRepository.findOne({where: {name}});
+
+    if(user){
+      if( user.password === password){
+        return user;
+      }
+    }
+    throw new Error('User incorrect');
+  }
+}
+
+export {UsersService}
